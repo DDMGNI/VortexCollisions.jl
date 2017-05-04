cc4842a1ede01c83d67a52a2ab3d4b30ab3b6741
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-05-04 10:03:37 +0200

    Fix grid tests.

--- test/test_grid.jl
+++ test/test_grid.jl
@@ -1,8 +1,9 @@
 
 function testGrid()
-    grid = Grid2d(7,6)
-    frequencies(1, 1, grid)
-    frequencies(2, 3, grid)
+    grid = Grid2d(7,7)
+    @assert frequencies(1, 1, grid) == ( 0,0)
+    @assert frequencies(2, 3, grid) == ( 1,2)
+    @assert frequencies(5, 4, grid) == (-3,3)
 end
 
 
