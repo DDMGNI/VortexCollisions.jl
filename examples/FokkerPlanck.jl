db3f433d5e88436c978e8ef91b613c4fa515ad6c
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-05-05 14:39:01 +0200

    Added some more examples to FokkerPlanck run script and energy diagnostic to notebook.

--- examples/FokkerPlanck.jl
+++ examples/FokkerPlanck.jl
@@ -1,4 +1,5 @@
 
+# to run in parallel set environment variable JULIA_NUM_THREADS to > 1
 if haskey(ENV, "JULIA_NUM_THREADS")
     nw = parse(Int, ENV["JULIA_NUM_THREADS"])
     addprocs(nw)
@@ -17,14 +18,22 @@ const N = 64
 const ℳcut = M
 const 𝒩cut = N
 
-const output = "FokkerPlanck.h5"
-
 
 const a = 1
 const b = 2
 
-function u_init(x,y)
+function u_init_exp_cos(x,y)
     exp(a*cos(x-π) + b*cos(y-π))
+end
+
+
+function u_init_sinx4(x,y)
+    sin(x)^4
+end
+
+
+function u_init_sin4x(x,y)
+    sin(4x)
 end
 
 
@@ -32,4 +41,13 @@ gr = Grid2d(M,N)
 ft = FourierTransform(gr, ℳcut=ℳcut, 𝒩cut=𝒩cut)
 op = FokkerPlanckOperator(gr, ft)
 
-run_simulation(op, nt, Δt, u_init, output)
+
+# test
+# run_simulation(op, 10, Δt, u_init_exp_cos, "FokkerPlanck_exp_cos_dt1E-6_nt10.h5")
+# run_simulation(op, 10, Δt, u_init_sinx4,   "FokkerPlanck_sinx4_dt1E-6_nt10.h5")
+# run_simulation(op, 10, Δt, u_init_sin4x,   "FokkerPlanck_sin4x_dt1E-6_nt10.h5")
+
+# production
+# run_simulation(op, nt, Δt, u_init_exp_cos, "FokkerPlanck_exp_cos_dt1E-6_nt" * string(nt) * ".h5")
+# run_simulation(op, nt, Δt, u_init_sinx4,   "FokkerPlanck_sinx4_dt1E-6_nt" * string(nt) * ".h5")
+# run_simulation(op, nt, Δt, u_init_sin4x,   "FokkerPlanck_sin4x_dt1E-6_nt" * string(nt) * ".h5")
